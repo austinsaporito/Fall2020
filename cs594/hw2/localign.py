f12814de055a88a7645a107406894d91be6a0f8d
austin <austincsp@yahoo.com>
2020-09-09 09:27:33 -0400

    just doing traceback string

--- cs594/hw2/localign.py
+++ cs594/hw2/localign.py
@@ -35,11 +35,11 @@ def main():
 
     submatrix[1][0]="y"
     submatrix[0][1]="y"
+
     #column
     for i in range(2,len(humangenome)+2):
         submatrix[i][0]=humangenome[i-2]
         submatrix[i-1][1]=total
-        total+=gap
 
     submatrix[i][1]=total
     total=0
@@ -47,12 +47,15 @@ def main():
     for i in range(2,len(muskitogenome)+2):
         submatrix[0][i]=muskitogenome[i-2]
         submatrix[1][i-1]=total
-        total+=gap
+        
     
     submatrix[1][i]=total
+    
     for i in range(len(submatrix)):
         directionmatrix[i]=submatrix[i].copy()
-
+    highesti=0
+    highestj=0
+    highest=0
     for i in range(len(submatrix)):
            for j in range(len(submatrix[i])):
                if submatrix[i][j] is None and i is not 0 and j is not 0:
@@ -61,7 +64,11 @@ def main():
                    left=submatrix[i][j-1]
                    left+=gap
                    above+=gap
-                    
+                   if above < 0:
+                       above=0
+                   if left < 0:
+                       left=0
+
                    if submatrix[0][j] is submatrix[i][0]:
                        diagnol+=match
                    elif submatrix[0][j] is not submatrix[i][0]:
@@ -70,14 +77,29 @@ def main():
                    if diagnol >= above and diagnol >= left:
                        submatrix[i][j]=diagnol
                        directionmatrix[i][j]="d"
+                       if diagnol > highest:
+                           highest=diagnol
+                           highesti=i
+                           highestj=j
                    elif above >= diagnol and above >= left:
                        submatrix[i][j]=above
                        directionmatrix[i][j]="a"
+                       if above > highest:
+                           highest=diagnol
+                           highesti=i
+                           highestj=j
                    elif left >= above and left >= diagnol: 
                        submatrix[i][j]=left
                        directionmatrix[i][j]="l"
+                       if left > highest:
+                           highest=diagnol
+                           highesti=i
+                           highestj=j
+                    
     humanlen=len(humangenome)
     muskitolen=len(muskitogenome)
+
+    print(submatrix[highesti][highestj])
 
     if humanlen > muskitolen:
         traceback=muskitolen
@@ -90,9 +112,13 @@ def main():
     i=len(submatrix)-1
     j=len(submatrix[0])-1
     traceback-=1
+    localignstring=""
     while traceback>=0:
+
         if directionmatrix[i][j] is "d":
             finalstring=tracebackstring[traceback]+finalstring
+            #if i <= highesti and j <= highestj:
+            #    localignstring=tracebackstring[traceback]+localignstring
             i-=1
             j-=1
         elif directionmatrix[i][j] is "a" :
@@ -105,6 +131,13 @@ def main():
         traceback-=1
     tmp=""
     tmp2=""
+    print(localignstring)
+    with open("localign.txt","a") as la:
+        la.write(finalstring+"\n")
+        la.write(humangenome+"\n")
+        la.write(localignstring+"\n")
+
+    '''
     for i in range(len(finalstring)):
         tmp+=finalstring[i]
         tmp2+=humangenome[i]
@@ -114,5 +147,8 @@ def main():
             print()
             tmp=""
             tmp2=""
+    '''
+
+
 if __name__=="__main__":
     main()
