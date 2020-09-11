fa4fde2f5fe45a399c2d40e23f4abaef1c7bf6b6
austin <austincsp@yahoo.com>
2020-09-10 22:11:37 -0400

    done with 7

--- cs594/hw2/egflinear.py
+++ cs594/hw2/egflinear.py
@@ -19,99 +19,74 @@ def main():
     humangenome=humangenome.replace("\n","")
     muskitogenome=muskitogenome.replace("\n","")
 
-    #humangenome=humangenome[:5]
-    #muskitogenome=muskitogenome[:5]
+    #humangenome=humangenome[:123]
+    #muskitogenome=muskitogenome[:123]
 
-    submatrix=[None]*(len(humangenome)+2)
-    directionmatrix=[None]*(len(humangenome)+2)
+    columnmatrix=[None]*(len(humangenome)+2)
 
-    for i in range(len(submatrix)):
-        submatrix[i]=[None]*(len(muskitogenome)+2)
+    rowmatrix=[None]*(len(muskitogenome)+2)
     gap=-2
     match=2
     mismatch=-1
-    total=0
 
-    submatrix[1][0]="y"
-    submatrix[0][1]="y"
-    maxcolumn=len(humangenome)+2
+    columnmatrix[1]="y"
+    rowmatrix[1]="y"
     #column
     for i in range(2,len(humangenome)+2):
-        submatrix[i][0]=humangenome[i-2]
-        submatrix[i-1][1]=total
+        columnmatrix[i]=humangenome[i-2]
 
-    submatrix[i][1]=total
-    total=0
-    maxrow=len(muskitogenome)+2
     #row
     for i in range(2,len(muskitogenome)+2):
-        submatrix[0][i]=muskitogenome[i-2]
-        submatrix[1][i-1]=total
-        
+        rowmatrix[i]=muskitogenome[i-2]
+
+    outermatrix=[None]*2
+    for i in range(2):
+        outermatrix[i]=[0]*(len(rowmatrix)-1)
     
-    submatrix[1][i]=total
-    #for i in submatrix:
-    #    print(i)
-    #exit()
-    
-    for i in range(len(submatrix)):
-        directionmatrix[i]=submatrix[i].copy()
-    highesti=0
-    highestj=0
-    highest=0
-    biggestcolumn=0
     biggestrow=0
+    biggestcolumn=0
+    for i in range(1,len(columnmatrix)):
+        for j in range(1,len(rowmatrix)):
+            diagnol=outermatrix[0][j-2]
+            above=outermatrix[0][j-1]
+            left=outermatrix[1][j-2]
+            left+=gap
+            above+=gap
+            if above < 0:
+                above=0
+            if left < 0:
+                left=0
+
+            if rowmatrix[j] is columnmatrix[i]:
+                diagnol+=match
+            elif rowmatrix[j] is not columnmatrix[i]:
+                diagnol+=mismatch
+            if j-1 != 0:
+                if diagnol >= above and diagnol >= left:
+                    outermatrix[1][j-1]=diagnol
+                elif above >= diagnol and above >= left:
+                    outermatrix[1][j-1]=above
+                elif left >= above and left >= diagnol: 
+                    outermatrix[1][j-1]=left
+
+            if j == len(rowmatrix)-1:
+                if outermatrix[1][j-1] >= biggestcolumn:
+                    biggestcolumn=outermatrix[1][j-1]
+                    #print(biggestcolumn)
+            if i == len(columnmatrix)-1:
+                if outermatrix[1][j-1] >= biggestrow:
+                    biggestrow=outermatrix[1][j-1]
+                    #print(biggestrow)
+        outermatrix[0]=outermatrix[1].copy()
+        for k in range(len(outermatrix[0])):
+            outermatrix[1][k]=0
+        #print(outermatrix[0])
+        #print(outermatrix[1])
+        #print()
+        #if i == 5:
+        #    exit()
 
 
-    for i in range(len(submatrix)):
-           for j in range(len(submatrix[i])):
-               if submatrix[i][j] is None and i is not 0 and j is not 0:
-                   diagnol=submatrix[i-1][j-1]
-                   above=submatrix[i-1][j]
-                   left=submatrix[i][j-1]
-                   left+=gap
-                   above+=gap
-                   if above < 0:
-                       above=0
-                   if left < 0:
-                       left=0
-
-                   if submatrix[0][j] is submatrix[i][0]:
-                       diagnol+=match
-                   elif submatrix[0][j] is not submatrix[i][0]:
-                       diagnol+=mismatch
-                   
-                   if diagnol >= above and diagnol >= left:
-                       submatrix[i][j]=diagnol
-                       directionmatrix[i][j]="d"
-                       if diagnol > highest:
-                           highest=diagnol
-                           highesti=i
-                           highestj=j
-                   elif above >= diagnol and above >= left:
-                       submatrix[i][j]=above
-                       directionmatrix[i][j]="a"
-                       if above > highest:
-                           highest=diagnol
-                           highesti=i
-                           highestj=j
-                   elif left >= above and left >= diagnol: 
-                       submatrix[i][j]=left
-                       directionmatrix[i][j]="l"
-                       if left > highest:
-                           highest=diagnol
-                           highesti=i
-                           highestj=j
-
-                   if j == len(submatrix[0])-1:
-                       if submatrix[i][j] >= biggestcolumn:
-                           biggestcolumn=submatrix[i][j]
-                   if i == len(submatrix)-1:
-                       if submatrix[i][j] >= biggestrow:
-                           biggestrow=submatrix[i][j]
-                    
-    humanlen=len(humangenome)
-    muskitolen=len(muskitogenome)
     if biggestrow > biggestcolumn:
         print(biggestrow)
     elif biggestcolumn > biggestrow:
