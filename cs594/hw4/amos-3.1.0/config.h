6d138e65f3cdbc6d5a922d8bf78f8f35fa3df465
austinsaporito <dxh594@hydra8.eecs.utk.edu>
2020-10-12 22:12:33 -0400

    more files

--- cs594/hw4/amos-3.1.0/config.h
+++ cs594/hw4/amos-3.1.0/config.h
@@ -203,7 +203,12 @@
 #endif
 
 /* Define to 1 if the X Window System is missing or not being used. */
-#define X_DISPLAY_MISSING 1
+/* #undef X_DISPLAY_MISSING */
+
+/* Enable large inode numbers on Mac OS X 10.5.  */
+#ifndef _DARWIN_USE_64_BIT_INODE
+# define _DARWIN_USE_64_BIT_INODE 1
+#endif
 
 /* Number of bits in a file offset, on hosts where this is settable. */
 /* #undef _FILE_OFFSET_BITS */
